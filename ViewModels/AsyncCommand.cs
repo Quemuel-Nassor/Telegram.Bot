c55cf4e9b892bb69c552d0dc3d403335a8028e70
Quemuel Nassor <quemuel.nassor@novasingular.com.br>
2026-03-16 20:10:16 -0300

    Add generic AsyncCommand implementation for asynchronous operations with parameters

--- ViewModels/AsyncCommand.cs
+++ ViewModels/AsyncCommand.cs
@@ -38,4 +38,43 @@ namespace Telegram.Bot.ViewModels
             }
         }
     }
+
+    /// <summary>
+    /// Implementação genérica de ICommand para operações assíncronas com parâmetro
+    /// </summary>
+    public class AsyncCommand<T> : ICommand
+    {
+        private readonly Func<T?, Task> _execute;
+        private readonly Predicate<T?>? _canExecute;
+        private bool _isExecuting;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public AsyncCommand(Func<T?, Task> execute, Predicate<T?>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => !_isExecuting && (_canExecute == null || _canExecute((T?)parameter));
+
+        public async void Execute(object? parameter)
+        {
+            if (!CanExecute(parameter))
+                return;
+
+            _isExecuting = true;
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+            try
+            {
+                await _execute((T?)parameter);
+            }
+            finally
+            {
+                _isExecuting = false;
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
 }
