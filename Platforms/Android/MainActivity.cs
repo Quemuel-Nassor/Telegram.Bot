ab80b5677c39eb543e220cc20a95c08d257f7a40
Quemuel Nassor <quemuel.nassor@novasingular.com.br>
2026-03-19 07:21:15 -0300

    fix: tabBar sobreposing android navigation buttons

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,10 +1,50 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using AndroidX.Core.View;
 
 namespace Telegram.Bot
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(
+        Theme = "@style/Maui.SplashTheme",
+        MainLauncher = true,
+        LaunchMode = LaunchMode.SingleTop,
+        ConfigurationChanges = ConfigChanges.ScreenSize
+            | ConfigChanges.Orientation
+            | ConfigChanges.UiMode
+            | ConfigChanges.ScreenLayout
+            | ConfigChanges.SmallestScreenSize
+            | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Platform.Init(this, savedInstanceState);
+
+            ConfigureWindowInsetsListener();
+        }
+
+        private void ConfigureWindowInsetsListener()
+        {
+            var insetsListener = new SystemBarsInsetsListener();
+            ViewCompat.SetOnApplyWindowInsetsListener(Window!.DecorView, insetsListener);
+        }
+
+        private class SystemBarsInsetsListener : Java.Lang.Object, IOnApplyWindowInsetsListener
+        {
+            public WindowInsetsCompat OnApplyWindowInsets(global::Android.Views.View view, WindowInsetsCompat insets)
+            {
+                var systemBars = insets.GetInsets(WindowInsetsCompat.Type.SystemBars());
+
+                view.SetPadding(
+                    left: 0,
+                    top: 0,
+                    right: 0,
+                    bottom: systemBars.Bottom);
+
+                return insets;
+            }
+        }
     }
 }
